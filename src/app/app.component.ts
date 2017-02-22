0c8a45841b20d843483ba1177fc2de6994336eb4
Kalpens <m.kalejs@outlook.com>
2017-02-22 11:22:56 +0100

    First real commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Course} from './course';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  courses : Course[];
+  constructor(){
+    this.courses = [
+      {
+        academy: 'EASV',
+        year: 2017,
+        title: 'Angular2',
+        education: 'Computer science',
+        lecturer: 'Lars bilde'
+      },
+      {
+        academy: 'EASV',
+        year: 2017,
+        title: 'ITO',
+        education: 'Computer science',
+        lecturer: 'Lars bilde'
+      }
+    ];
+  }
 }
